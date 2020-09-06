7e874b963cc7b272012ee41728f59994129f578f
Sven Mischkewitz <sven.mkw@gmail.com>
2020-09-06 19:31:39 +0200

    Automatically launch emacs daemon on OSX via launchctl

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
 set -e
-source "$(dirname $0)/../scripts/utils.sh"
+source "$(dirname "$0")/../scripts/utils.sh"
+
+PLATFORM="$(uname -s)"
 
 if command -v emacs >/dev/null 2>&1; then
   info 'emacs is already installed'
 elif ! command -v brew >/dev/null 2>&1; then
-  error 'brew is not installed, cannot install emacs'
+  fail 'brew is not installed, cannot install emacs'
   return 1
 else
   brew cask install emacs
+fi
+
+if [ "$PLATFORM" = "Darwin" ]; then
+  link_file "$(dirname "$0")/emacs.daemon.plist" ~/Library/LaunchAgents/emacs.daemon.plist
+  info 'loading emacs daemon service'
+  launchctl load -w ~/Library/LaunchAgents/emacs.daemon.plist
+  launchctl start emacs.daemon
+else
+  fail 'no daemon launcher installed'
 fi
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+#
+# Helpers for printing nicely formatted progress updates
+#
+
 info() {
   # shellcheck disable=SC2059
   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
@@ -20,4 +24,26 @@ fail() {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit
+}
+
+#
+# Copy and symlink utils
+#
+
+# Create a symlink. If the symlink already exists, skip the action. If another
+# file exists with the same name, create a backup and then create the symlink.
+link_file() {
+  # $1 - source
+  # $2 - destination
+  if [ -e "$2" ]; then
+    if [ "$(readlink "$2")" = "$1" ]; then
+      success "skipped $1"
+      return 0
+    else
+      mv "$2" "$2.backup"
+      success "moved $2 to $2.backup"
+    fi
+  fi
+  ln -sf "$1" "$2"
+  success "linked $1 to $2"
 }
