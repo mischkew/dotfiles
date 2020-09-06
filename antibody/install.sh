0321c359617a25c4df48c59b10a4059042bcfc42
Sven Mischkewitz <sven.mkw@gmail.com>
2020-09-06 18:42:38 +0200

    Fix install check for antibody

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,7 +3,7 @@
 set -e
 source "$(dirname $0)/../scripts/utils.sh"
 
-if command -v brew >/dev/null 2>&1; then
+if command -v antibody >/dev/null 2>&1; then
   info 'antibody is already installed'
 else
   curl -sL https://git.io/antibody | sh -s
