2bfcd5223870acb74b1cbe9dc1b26e4ec99e87f1
Sven Mischkewitz <sven.mkw@gmail.com>
2020-09-14 22:33:33 +0200

    Allow editing of command lines in emacs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,3 +6,9 @@ SAVEHIST=10000 # store past <SAVEHIST> commands to disk
 HISTFILE=~/.history # store history into <HISTFILE>
 setopt SHARE_HISTORY # share command history between multiple shells
 setopt HIST_FIND_NO_DUPS # don't search for duplicate commands with C-r
+
+# allow editing of commands in emacs
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^xe' edit-command-line
+bindkey '^x^e' edit-command-line
