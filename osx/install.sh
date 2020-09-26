ff2d46d8182f67e65f4d78f2a332823924f40407
Sven Mischkewitz <sven.mkw@gmail.com>
2020-09-26 17:58:41 +0200

    Install Rectangle.app on OSX for window shortcuts

---
+++ osx/install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+source "$(dirname $0)/../scripts/utils.sh"
+
+platform=$(uname -s)
+
+install_osx_packages() {
+    info "Install casks for OSX"
+
+    # Keyboard Shortcuts for the OSX window manager
+    # https://github.com/rxhanson/Rectangle
+    brew cask install rectangle
+
+    user "Make sure to set Rectangle to launch automatically on boot"
+    user "Make sure to set the accessibility rights for Rectangle!"
+}
+
+if [ "$platform" = "Darwin" ]; then
+    install_osx_packages
+fi
