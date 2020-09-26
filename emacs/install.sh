f1b16cd12e37f8dcb3e987e177090c593849dff0
Sven Mischkewitz <sven.mkw@gmail.com>
2020-09-26 19:52:22 +0200

    Install shellcheck flycheck for emacs

--- emacs/install.sh
+++ emacs/install.sh
@@ -5,14 +5,30 @@ source "$(dirname "$0")/../scripts/utils.sh"
 
 PLATFORM="$(uname -s)"
 
+#
+# install emacs
+#
+
 if command -v emacs >/dev/null 2>&1; then
   info 'emacs is already installed'
 elif ! command -v brew >/dev/null 2>&1; then
   fail 'brew is not installed, cannot install emacs'
   return 1
-else
+elif [ "$PLATFORM" = "Darwin" ]; then
   brew cask install emacs
+else
+  fail "I do not know how to install emacs on your platform (${PLATFORM})!"
 fi
+
+#
+# install auxiliary packages for emacs usage
+#
+
+brew install shellcheck
+
+#
+# setup emacs daemon
+#
 
 if [ "$PLATFORM" = "Darwin" ]; then
   link_file "$(dirname "$0")/emacs.daemon.plist" ~/Library/LaunchAgents/emacs.daemon.plist
