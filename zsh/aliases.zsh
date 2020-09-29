0dbf01df24f84a1dbe984a95c7ada2152c147437
Sven Mischkewitz <sven.mkw@gmail.com>
2020-09-29 12:59:48 +0200

    Add watch alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias zshconfig="$EDITOR $HOME/.zshrc"
 alias ec="emacsclient -nw -a 'emacs -nw'"
 alias ls-ports="netstat -ltnp"
 alias ll="ls -al"
+alias watch="ffplay -i"
 
 if [ "$PLATFORM" = "Linux" ]; then
   alias open="xdg-open"
