96030e4cb7017fcb0386e355df3b755ec8d9b55e
Sven Mischkewitz <sven.mkw@gmail.com>
2020-10-10 22:28:43 +0200

    Setup emacsclient for GUI and TTY

---
+++ emacs/aliases.zsh
@@ -0,0 +1,17 @@
+#! /bin/zsh
+
+# find the socket of the running emacs daemon by listing all open file
+# connections (aka sockets), find files of emacs of the current user
+# with the specific `system-server` daemon name. The system-server is
+# launched by the launchctl/ systemd configuration of these
+# dotfiles. We then pick the filename which is the last column of lsof
+# via awk.
+# see: https://stackoverflow.com/a/28553846
+server_file="$(lsof -c /emacs/i | grep $USER | grep system-server | awk 'NR==1 {print $NF}')"
+
+# use the GUI emacs, try to reuse the current frame, don't block the
+# tty
+alias ec="emacsclient -s '$server_file' -a 'emacs'"
+
+# use emacs in the tty
+alias ecc="emacsclient -s '$server_file' -nw -a 'emacs -nw'"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,6 @@
 #! /bin/zsh
 
 alias zshconfig="$EDITOR $HOME/.zshrc"
-alias ec="emacsclient -nw -a 'emacs -nw'"
 alias ls-ports="netstat -ltnp"
 alias ll="ls -al"
 alias watch="ffplay -i"
@@ -10,10 +9,3 @@ if [ "$PLATFORM" = "Linux" ]; then
   alias open="xdg-open"
   alias pbcopy="xclip -sel clip"
 fi
-
-#
-# ThinkSono-related
-#
-
-alias tstk="pyenv activate tstk &> /dev/null && tstk"
-alias medconv="pyenv activate medconv &> /dev/null && medconv"
