fe4c26712e8ac4d8daa8a3ce42b6260992cd7f11
Sven Mischkewitz <sven.mkw@gmail.com>
2020-10-22 19:10:49 +0200

    Install pyenv via homebrew on linux

--- python/install.sh
+++ python/install.sh
@@ -3,8 +3,32 @@
 set -e
 source "$(dirname $0)/../scripts/utils.sh"
 
+PLATFORM="$(uname -s)"
+
+if [ "$PLATFORM" = "Linux" ]; then
+  eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+  # install python package dependencies
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    llvm \
+    libncurses5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
+fi
+
 if command -v pyenv >/dev/null 2>&1; then
   info 'pyenv is already installed'
 else
-  brew install pyenv
+  brew install pyenv pyenv-virtualenv
 fi
