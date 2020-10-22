7aba437e6c361d8c59a96d4d69dd3892f575bae4
Sven Mischkewitz <sven.mkw@gmail.com>
2020-10-22 19:09:45 +0200

    Fail brew install when curl is missing

--- brew/install.sh
+++ brew/install.sh
@@ -3,8 +3,17 @@
 set -e
 source "$(dirname $0)/../scripts/utils.sh"
 
+PLATFORM="$(uname -s)"
+
+if [ "$PLATFORM" = "Linux" ]; then
+  eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+fi
+
 if command -v brew >/dev/null 2>&1; then
   info 'brew is already installed'
+elif ! command -v curl >/dev/null 2>&1; then
+  fail 'curl not found'
+  exit 1
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
