a48ae336899df6e0d00ac287548d2c87fc201aca
Sven Mischkewitz <sven.mkw@gmail.com>
2020-10-22 19:32:14 +0200

    Install z extension without antibody

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+source "$(dirname $0)/../scripts/utils.sh"
 PLATFORM="$(uname -s)"
 
 if [ "$PLATFORM" = "Linux" ]; then
@@ -14,4 +15,14 @@ elif ! command -v brew >/dev/null 2>&1; then
   fail 'brew is not installed'
 else
   brew install zsh
+fi
+
+# install zsh extensions 
+
+if command -v z >/dev/null 2>&1; then
+  info '\t z extension already installed'
+else
+  info '\t installing z extension'
+  git submodule update --init "$(dirname $0)/z"
+  touch "$HOME/.z"
 fi
