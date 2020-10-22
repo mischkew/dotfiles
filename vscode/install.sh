0038f48ca794ea2a697382aac05336e07287648a
Sven Mischkewitz <sven.mkw@gmail.com>
2020-10-22 19:12:30 +0200

    Resolve dotfiles root for vscode installer correctly

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+DOTFILES_ROOT="$(dirname $0)/.."
+
 if command -v code >/dev/null; then
   if [ "$(uname -s)" = "Darwin" ]; then
     VSCODE_HOME="$HOME/Library/Application Support/Code"
@@ -7,11 +10,14 @@ if command -v code >/dev/null; then
   fi
   mkdir -p "$VSCODE_HOME/User"
 
-  ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
-  ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
-  ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+  ln -sf "$DOTFILES_ROOT/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+  ln -sf "$DOTFILES_ROOT/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
+  ln -sf "$DOTFILES_ROOT/vscode/snippets" "$VSCODE_HOME/User/snippets"
 
   while read -r module; do
     code --install-extension "$module" || true
-  done <"$DOTFILES/vscode/extensions.txt"
+  done <"$DOTFILES_ROOT/vscode/extensions.txt"
+else
+  fail "vscode not found"
+  exit 1
 fi
