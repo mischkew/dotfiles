1cf54c3c4b0519beb1212892c8e0ee96c8d6917f
Sven Mischkewitz <sven.mkw@gmail.com>
2020-11-17 22:54:04 +0100

    Connect to emacs server when editing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,13 @@
 #! /bin/zsh
 
+alias edit="$EDITOR"
+alias ec="emacsclient --tty"
 alias zshconfig="$EDITOR $HOME/.zshrc"
 alias ls-ports="netstat -ltnp"
 alias ll="ls -al"
 alias watch="ffplay -i"
 
+# "$PLATFORM" defined in .zshrc.symlink
 if [ "$PLATFORM" = "Linux" ]; then
   alias open="xdg-open"
   alias pbcopy="xclip -sel clip"
