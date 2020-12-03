0208b2ac8a4fb815ef6b28b33ef8f9d3ac49125a
Sven Mischkewitz <sven.mkw@gmail.com>
2020-12-03 22:26:18 +0100

    Use simpler ec and edit aliases for emacs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,32 @@
 #! /bin/zsh
 
-alias edit="$EDITOR"
-alias ec="emacsclient --tty"
-alias zshconfig="$EDITOR $HOME/.zshrc"
-alias ls-ports="netstat -ltnp"
 alias ll="ls -al"
+
+# Will connect to the running emacs GUI if called from a standard
+# terminal. Will connect to vscode if called from within a vscode
+# terminal
+alias edit="$EDITOR"
+
+# Will connect to a running emacs server but stay on the terminal.
+# Useful if using a terminal outside of emacs vterm (i.e. Gnome
+# Terminal or iTerm2)
+alias ec="emacsclient --tty"
+
+# Edit .zshrc in the standard editor
+alias zshconfig="$EDITOR $HOME/.zshrc"
+
+# List all ports which are currently listening to applications, useful
+# to debug if ports are blocked.
+alias ls-ports="netstat -ltnp"
+
+# Use ffmpeg to watch a video from the command line.
 alias watch="ffplay -i"
 
 # "$PLATFORM" defined in .zshrc.symlink
 if [ "$PLATFORM" = "Linux" ]; then
-  alias open="xdg-open"
-  alias pbcopy="xclip -sel clip"
+    # Open anything with open, like on OSX.
+    alias open="xdg-open"
+
+    # Copy to clipboard with the same command as on OSX.
+    alias pbcopy="xclip -sel clip"
 fi
