a2bad94b609200faf4786a9a7f74171571dca117
Sven Mischkewitz <sven.mkw@gmail.com>
2020-12-14 22:36:44 +0100

    Follow symlinks to fix zsh setup

--- python/path.zsh
+++ python/path.zsh
@@ -1,10 +1,3 @@
 #! /bin/zsh
 
-# This lazy-loads pyenv by executing the pyenv init command first call, which
-# then essentially replaces this function
-# See: https://carlosbecker.com/posts/speeding-up-zsh
-pyenv() {
-  eval "$(command pyenv init -)"
-  eval "$(command pyenv virtualenv-init -)"
-  pyenv "$@"
-}
+eval "$(command pyenv init - --no-rehash)"
