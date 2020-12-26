3653f9c8e1609dbd3168f367d5ba57cfb6805d27
Sven Mischkewitz <sven.mkw@gmail.com>
2020-12-26 20:34:50 +0100

    Build llvm toolchain on Linux

--- c/install.sh
+++ c/install.sh
@@ -7,13 +7,29 @@ PLATFORM="$(uname -s)"
 
 
 if [ "$PLATFORM" = "Darwin" ]; then
-  info 'installing ccls, llvm and bear'
+  info 'installing llvm and bear'
   if ! command -v brew >& /dev/null; then
-	  fail 'brew is not installed, cannot install ccls'
+	  fail 'brew is not installed, cannot install llvm or bear'
 	  return 1
   else
-	  brew install ccls llvm bear
+	  brew install llvm bear
   fi
+elif [ "$PLATFORM" = "Linux" ]; then
+  info 'installing and building llvm'
+  WORKING_DIR="$(realpath "$(dirname "$0")")"
+  cd "$WORKING_DIR"
+  git submodule update --init "./llvm-project"
+  mkdir -p ./llvm-project/build
+  cd ./llvm-project/build
+  cmake ../llvm -G "Unix Makefiles" \
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lldb" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="$WORKING_DIR" \
+    && make -j 8 \
+    && make install
+
+  info 'installing bear'
+  sudo apt install bear
 else
-  fail "don't know how to install ccls no your platform"
+  fail "don't know how to install llvm no your platform"
 fi
--- c/paths.zsh
+++ c/paths.zsh
@@ -3,4 +3,7 @@
 if [ "$PLATFORM" = "Darwin" ]; then
   # use homebrew llvm toolchain instead of the system one
   export PATH="/usr/local/opt/llvm/bin:${PATH}"
+elif [ "$PLATFORM" = "Linux" ]; then
+  # self-built llvm toolchain and Bear tool
+  export PATH="${DOTFILES}/c/bin:${PATH}"
 fi
