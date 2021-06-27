db41b4009804ab32c480e534054b14aaf9287d03
Sven Mischkewitz <sven.mkw@gmail.com>
2021-06-27 11:21:00 +0200

    Speed up pyenv setup on OSX by avoiding completions

--- python/path.zsh
+++ python/path.zsh
@@ -1,3 +1,26 @@
 #! /bin/zsh
 
-eval "$(command pyenv init - --no-rehash)"
+export PATH="${HOME}/.pyenv/bin:${PATH}"
+
+if [ "$PLATFORM" = "Linux" ]; then
+  eval "$(command pyenv init - --no-rehash)"
+elif [ "$PLATFORM" = "Darwin" ]; then
+  export PATH="/Users/sven/.pyenv/shims:${PATH}"
+  export PYENV_SHELL=zsh
+  pyenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+      activate|deactivate|rehash|shell)
+        eval "$(pyenv "sh-$command" "$@")";;
+      *)
+        command pyenv "$command" "$@";;
+    esac
+  }
+else
+  echo "Don't know how to setup pyenv on your platform: $PLATFORM"
+fi
